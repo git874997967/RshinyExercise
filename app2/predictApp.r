068ca76c3917311efb985c4c7cf339f513b2d9bb
git874997967 <g874997967@gmail.com>
2017-12-22 00:17:01 -0800

    finish pageLayout next day is focusing the data connect

--- app2/predictApp.r
+++ app2/predictApp.r
@@ -1,6 +1,12 @@
 library(shiny)
 library(png)
- 
+cutInfo = read.csv('cut.csv',
+                   stringsAsFactors = F,
+                   encoding = 'UTF-8')
+colorInfo = read.csv('color.csv', stringsAsFactors = F)
+clarityInfo = read.csv('clarity.csv', stringsAsFactors = F)
+certInfo = read.csv('cert.csv', stringsAsFactors = F)
+
 ui <- fluidPage(
   titlePanel("Predictions on diamond price"),
   fluidRow(
@@ -77,7 +83,7 @@ ui <- fluidPage(
           label = 'Cut',
           list   (
             "Good" = "Good",
-            "Very Good" = "V.Good",
+            "Very Good" = "Very Good",
             "Ideal" = "Ideal"
           )
         )
@@ -89,7 +95,8 @@ ui <- fluidPage(
                  label = 'Color',
                  list   (
                    "D" = "D",
-                   "E" = "F",
+                   "E" = "E",
+                   "F" = "F",
                    "G" = "G",
                    "H" = "H",
                    "I" = "I",
@@ -110,8 +117,8 @@ ui <- fluidPage(
                    "VVS2" = "VVS2",
                    "VS1" = "VS1",
                    "VS2" = "VS2",
-                   "ST1" = "ST1",
-                   "ST2" = "ST2",
+                   "SI1" = "SI1",
+                   "SI2" = "SI2",
                    "I1" = "I1",
                    "I2" = "I2"
                  )
@@ -128,54 +135,203 @@ ui <- fluidPage(
                    "EGL" = "EGL",
                    "IGI" = "IGI",
                    "HRD" = "HRD",
-                   "EGL" = "EGL",
-                   "USA" = "USA",
+                   "EGL USA" = "EGL USA",
                    "EGL INIFI" = "EGL INIFI",
                    "EGL ISRAEL" = "EGL ISRAEL"
                  )
                )
-               
              ))
     ),
-    fluidRow(
-      column(1,
-             imageOutput(
-               outputId = "image1",
-               height = 200,
-               width = 200
-             )),
-      column(1,
-             imageOutput(
-               outputId = "image2",
-               height = 200,
-               width = 200
-             )),
-      column(1,
-             imageOutput(
-               outputId = "image3",
-               height = 200,
-               width = 200
-             )),
-      column(1,
-             imageOutput(
-               outputId = "image4",
-               height = 200,
-               width =200
-             )),
-      column(8, wellPanel(
-        h2("Predict results:"),
-        tableOutput(outputId = 'diamondInfo'),
-        tableOutput(outputId = 'results')
-      ))
-      
-    )
+    fluidRow(column(6,
+                    fluidRow(
+                      wellPanel(
+                        tags$div(
+                        class = 'media',
+                        tags$div(class='media-left',
+                        imageOutput(
+                          outputId = "cutImg",
+                          width = "100%",
+                          height = "100%"
+                        )),
+                        tags$div(class = 'media-body',
+                                h3(textOutput("cut")),
+                                 h4(textOutput('cutDesc')))
+                        
+                      ))
+                    )),
+             column(6,
+                    fluidRow(
+                      wellPanel(
+                        tags$div(
+                          class = 'media',
+                          tags$div(class='media-left',
+                          imageOutput(
+                            outputId = "colorImg",
+                            width = "100%",
+                            height = "100%"
+                          )),
+                          tags$div(class = 'media-body',
+                                   h3(textOutput("color")),
+                                    h4(textOutput('colorDesc')))
+                          
+                        ))
+                    ))),
+    fluidRow(column(6,
+                    fluidRow(
+                      wellPanel(
+                        tags$div(
+                          class = 'media',
+                          tags$div(class='media-left',
+                          imageOutput(
+                            outputId = "clarityImg",
+                            width = "100%",
+                            height = "100%"
+                          )),
+                          tags$div(class = 'media-body',
+                                   h3(textOutput("clarity")),
+                                    h4(textOutput('clarityDesc')))
+                          
+                        ))
+                    )),
+             column(6,
+                    fluidRow(
+                      wellPanel(
+                        tags$div(
+                          class = 'media',
+                          tags$div(class='media-left',
+                                  imageOutput(
+                            outputId = "certImg",
+                            width = "100%",
+                            height = "100%"
+                          )),
+                         
+                          tags$div(class = 'media-body',
+                                   h3(textOutput("cert")),
+                                    h4(textOutput('certDesc')))
+                          
+                        ))
+                    )))
   )
 )
 
-
-
-
 server <- function(input, output, session) {
+  #cut rendering
+  output$cut <- renderText({
+    paste("Cut:", input$cut, seq = " ")
+  })
+  cutColumn = reactive({
+    switch(
+      input$cut,
+      "Good" = 1,
+      "Very Good" = 2,
+      "Ideal" = 3
+    )
+  })
+  output$cutDesc = renderText({
+    cutInfo[1, cutColumn() + 1]
+  })
+  output$cutImg <-renderImage({
+    return(list(
+      src =cutInfo[2,cutColumn()+1],
+      contentType = "image/png",
+      alt = cutInfo[2,cutColumn()+1]
+    ))
+
+  },deleteFile = FALSE)
+  #color rendering
+  output$color = renderText({
+    paste("Color:", input$color, seq = ' ')
+  })
+  colorColumn = reactive({
+    switch(
+      input$color,
+      "D" = 1,
+      "E" = 2,
+      "F" = 3,
+      "G" = 4,
+      "H" = 5,
+      "I" = 6,
+      "J" = 7,
+      "K" = 8,
+      "L" = 9
+      
+    )
+  })
+  output$colorDesc = renderText({
+    colorInfo[1, colorColumn() + 1]
+  })
+  output$colorImg <-renderImage({
+    return(list(
+      src =colorInfo[2,colorColumn()+1],
+      contentType = "image/png",
+      alt = colorInfo[2,colorColumn()+1]
+    ))
+    
+  },deleteFile = FALSE)
+  #clarity rendering
+  output$clarity = renderText({
+    paste("Clarity:", input$clarity, seq = ' ')
+  })
+  clarityColumn = reactive({
+    switch(
+      input$clarity,
+      "IF" = 1,
+      "VVS1" = 2,
+      "VVS2" = 3,
+      "VS1" = 4,
+      "VS2" = 5,
+      "SI1" = 6,
+      "SI2" = 7,
+      "I1" = 8,
+      "I2" = 9
+      
+    )
+  })
+  output$clarityDesc = renderText({
+    clarityInfo[1, clarityColumn() + 1]
+  })
+  
+  output$clarityImg <-renderImage({
+    return(list(
+      src =clarityInfo[2,clarityColumn()+1],
+      contentType = "image/png",
+      alt = clarityInfo[2,clarityColumn()+1]
+    ))
+    
+  },deleteFile = FALSE)
+  
+  
+  # cert rendering
+  output$cert = renderText({
+    paste("Cert:", input$cert, seq = ' ')
+  })
+  certColumn = reactive({
+    switch(
+      input$cert,
+      "GIA" = 1,
+      "AGS" = 2,
+      "EGL" = 3,
+      "IGI" = 4,
+      "HRD" = 5,
+      "EGL USA" = 6,
+      "EGL INIFI" = 7,
+      "EGL ISRAEL" = 8
+      
+    )
+  })
+  output$certDesc = renderText({
+    certInfo[1, certColumn() + 1]
+  })
+  
+  output$certImg <-renderImage({
+    return(list(
+      src =certInfo[2,certColumn()+1],
+      contentType = "image/png",
+      alt = certInfo[2,certColumn()+1]
+    ))
+    
+  },deleteFile = FALSE)
+  
   output$diamondInfo = renderTable({
     diamondTest = data.frame(
       input$carat,
@@ -211,39 +367,6 @@ server <- function(input, output, session) {
     results
     ## collectdata finished lack of chose should be done later
   })
-  
-  output$image1 <- renderImage({
-    return(list(
-      src = "www/rstudio.png",
-      contentType = "image/png",
-      alt = "Face"
-    ))
-    
-  }, deleteFile = FALSE)
-  output$image2 <- renderImage({
-    return(list(
-      src = "www/rstudio.png",
-      contentType = "image/png",
-      alt = "Face"
-    ))
-    
-  }, deleteFile = FALSE)
-  output$image3 <- renderImage({
-    return(list(
-      src = "www/rstudio.png",
-      contentType = "image/png",
-      alt = "Face"
-    ))
-    
-  }, deleteFile = FALSE)
-  output$image4 <- renderImage({
-    return(list(
-      src = "www/rstudio.png",
-      contentType = "image/png",
-      alt = "Face"
-    ))
-    
-  }, deleteFile = FALSE)
   
 }
 
--- predictApp.r
+++ predictApp.r
@@ -1,6 +1,11 @@
 library(shiny)
 library(png)
-mpgData <- mtcars
+cutInfo = read.csv('app2/cut.csv',
+                   stringsAsFactors = F,
+                   encoding = 'UTF-8')
+colorInfo = read.csv('app2/color.csv', stringsAsFactors = F)
+clarityInfo = read.csv('app2/clarity.csv', stringsAsFactors = F)
+certInfo = read.csv('app2/cert.csv', stringsAsFactors = F)
 
 ui <- fluidPage(
   titlePanel("Predictions on diamond price"),
@@ -72,25 +77,26 @@ ui <- fluidPage(
              )
            )),
     fluidRow(
-      column(2, wellPanel(
+      column(3, wellPanel(
         selectInput(
           inputId = 'cut',
           label = 'Cut',
           list   (
             "Good" = "Good",
-            "Very Good" = "V.Good",
+            "Very Good" = "Very Good",
             "Ideal" = "Ideal"
           )
         )
       )),
-      column(2,
+      column(3,
              wellPanel(
                selectInput(
                  inputId = 'color',
                  label = 'Color',
                  list   (
                    "D" = "D",
-                   "E" = "F",
+                   "E" = "E",
+                   "F" = "F",
                    "G" = "G",
                    "H" = "H",
                    "I" = "I",
@@ -100,7 +106,7 @@ ui <- fluidPage(
                  )
                )
              )),
-      column(2,
+      column(3,
              wellPanel(
                selectInput(
                  inputId = 'clarity',
@@ -111,14 +117,14 @@ ui <- fluidPage(
                    "VVS2" = "VVS2",
                    "VS1" = "VS1",
                    "VS2" = "VS2",
-                   "ST1" = "ST1",
-                   "ST2" = "ST2",
+                   "SI1" = "SI1",
+                   "SI2" = "SI2",
                    "I1" = "I1",
                    "I2" = "I2"
                  )
                )
              )),
-      column(2,
+      column(3,
              wellPanel(
                selectInput(
                  inputId = 'cert',
@@ -129,54 +135,141 @@ ui <- fluidPage(
                    "EGL" = "EGL",
                    "IGI" = "IGI",
                    "HRD" = "HRD",
-                   "EGL" = "EGL",
-                   "USA" = "USA",
+                   "EGL USA" = "EGL USA",
                    "EGL INIFI" = "EGL INIFI",
                    "EGL ISRAEL" = "EGL ISRAEL"
                  )
                )
-               
              ))
     ),
-    fluidRow(
-      column(1,
-             imageOutput(
-               outputId = "image1",
-               height = 200,
-               width = 200
-             )),
-      column(1,
-             imageOutput(
-               outputId = "image2",
-               height = 200,
-               width = 200
-             )),
-      column(1,
-             imageOutput(
-               outputId = "image3",
-               height = 200,
-               width = 200
-             )),
-      column(1,
-             imageOutput(
-               outputId = "image4",
-               height = 200,
-               width =200
-             )),
-      column(8, wellPanel(
-        h2("Predict results:"),
-        tableOutput(outputId = 'diamondInfo'),
-        tableOutput(outputId = 'results')
-      ))
-      
-    )
+    fluidRow(column(7,
+                    fluidRow(
+                      wellPanel(
+                        tags$div(
+                          class = 'media',
+                          imageOutput(
+                            outputId = "cutImg",
+                            width = "100%",
+                            height = "80%"
+                          ),
+                          tags$div(class = 'media-body',
+                                   h3(textOutput("cut")),
+                                   p(textOutput('cutDesc')))
+                          
+                        ))
+                    )))
   )
 )
 
-
-
-
 server <- function(input, output, session) {
+  #cut rendering
+  output$cut <- renderText({
+    paste("Cut:", input$cut, seq = " ")
+  })
+  cutColumn = reactive({
+    switch(
+      input$cut,
+      "Good" = 1,
+      "Very Good" = 2,
+      "Ideal" = 3
+    )
+  })
+  output$cutDesc = renderText({
+    cutInfo[1, cutColumn() + 1]
+  })
+  output$cutImg <-renderImage({
+    return(list(
+      src =paste('app2/',cutInfo[2,cutColumn()+1],sep=''),
+      
+      contentType = "image/png",
+      alt = paste('app2/',cutInfo[2,cutColumn()+1],sep='')
+    ))
+    
+  },deleteFile = FALSE)
+  # output$cutImg <- renderImage({
+  #   return(list(
+  #     src = "app2/www/cut_VeryGood.png",
+  #     contentType = "image/png",
+  #     alt = "Face"
+  #   ))
+  #   
+  # }, deleteFile = FALSE)
+  
+  #color rendering
+  output$color = renderText({
+    paste("Color:", input$color, seq = ' ')
+  })
+  colorColumn = reactive({
+    switch(
+      input$color,
+      "D" = 1,
+      "E" = 2,
+      "F" = 3,
+      "G" = 4,
+      "H" = 5,
+      "I" = 6,
+      "J" = 7,
+      "K" = 8,
+      "L" = 9
+      
+    )
+  })
+  output$colorDesc = renderText({
+    colorInfo[1, colorColumn() + 1]
+  })
+  # output$colorImg <- renderImage({
+  #   return(list(
+  #     src = "www/rstudio.png",
+  #     contentType = "image/png",
+  #     alt = "Face"
+  #   ))
+  #   
+  # }, deleteFile = FALSE)
+  #clarity rendering
+  output$clarity = renderText({
+    paste("Clarity:", input$clarity, seq = ' ')
+  })
+  clarityColumn = reactive({
+    switch(
+      input$clarity,
+      "IF" = 1,
+      "VVS1" = 2,
+      "VVS2" = 3,
+      "VS1" = 4,
+      "VS2" = 5,
+      "SI1" = 6,
+      "SI2" = 7,
+      "I1" = 8,
+      "I2" = 9
+      
+    )
+  })
+  output$clarityDesc = renderText({
+    clarityInfo[1, clarityColumn() + 1]
+  })
+  # cert rendering
+  output$cert = renderText({
+    paste("Cert:", input$cert, seq = ' ')
+  })
+  certColumn = reactive({
+    switch(
+      input$cert,
+      "GIA" = 1,
+      "AGS" = 2,
+      "EGL" = 3,
+      "IGI" = 4,
+      "HRD" = 5,
+      "EGL USA" = 6,
+      "EGL INIFI" = 7,
+      "EGL ISRAEL" = 8
+      
+    )
+  })
+  output$certDesc = renderText({
+    certInfo[1, certColumn() + 1]
+  })
+  
+  
   output$diamondInfo = renderTable({
     diamondTest = data.frame(
       input$carat,
@@ -212,39 +305,6 @@ server <- function(input, output, session) {
     results
     ## collectdata finished lack of chose should be done later
   })
-  
-  output$image1 <- renderImage({
-    return(list(
-      src = "www/rstudio.png",
-      contentType = "image/png",
-      alt = "Face"
-    ))
-    
-  }, deleteFile = FALSE)
-  output$image2 <- renderImage({
-    return(list(
-      src = "www/rstudio.png",
-      contentType = "image/png",
-      alt = "Face"
-    ))
-    
-  }, deleteFile = FALSE)
-  output$image3 <- renderImage({
-    return(list(
-      src = "www/rstudio.png",
-      contentType = "image/png",
-      alt = "Face"
-    ))
-    
-  }, deleteFile = FALSE)
-  output$image4 <- renderImage({
-    return(list(
-      src = "www/rstudio.png",
-      contentType = "image/png",
-      alt = "Face"
-    ))
-    
-  }, deleteFile = FALSE)
   
 }
 
